d8a3cccd2d1d4b7d9c46ee483be54758c4a1073b
Admin <ufukkirmizigedik1984@gmail.com>
2025-01-20 10:18:15 +0300

    updated

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from django.conf import settings
+
 def redirect_to_login(request):
     return redirect('login')
 
@@ -13,7 +14,7 @@ urlpatterns = [
     path('ufuk-ben-admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # accounts/views.py kullanılır
     path('app/', include('app.urls')),           # app/views.py kullanılır
-    path('', redirect_to_register),  # Ana sayfa login'e yönlendirir
+    path('', redirect_to_login),  # Ana sayfa login'e yönlendirir
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
